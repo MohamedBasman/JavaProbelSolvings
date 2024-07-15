dabb3c114451e99da4b994c4dbbc43457d1a2f0e
MohamedBasman <210701160@rajalakshmi.edu.in>
2024-07-15 16:17:09 +0530

    logical correction

---
+++ Fibonacci.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = s.nextInt();
+        int a = 0, b = 1, sum = 0;
+        
+        if (n < 0) {
+            System.out.println("Invalid input, please enter a non-negative number.");
+        } else if (n == 0) {
+            System.out.println(a);
+        } else if (n == 1) {
+            System.out.println(b);
+        } else {
+            for (int i = 2; i <= n; i++) {
+                sum = a + b;
+                a = b;
+                b = sum;
+            }
+            System.out.println(sum);
+        }
+        s.close();
+    }
+}
+
